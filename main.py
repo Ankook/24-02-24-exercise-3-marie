38f80a005e171e695b0de28903b1039062ed5b55
Ankook <52046510+Ankook@users.noreply.github.com>
2024-02-24 20:56:42 +0300

    commenting function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import string
 
 root = Tk()
 root.geometry("300x250+400+200")
@@ -24,16 +25,19 @@ def get_names():
     output_file_name = outputFileName_entry.get()
     print('output file name: ' + output_file_name)
     print('name output has ended')
-    working_with_files(input_file_name, output_file_name)
+    #working_with_files(input_file_name, output_file_name)
     #TODO разобраться с асинхронностью здесь
-def working_with_files(input_file_name, output_file_name):
-    inputFile = open(input_file_name, 'r')
 
-    for line in inputFile:
-        
-
-
-    ouputFile = open(output_file_name, 'w')
+    
+#def working_with_files(input_file_name, output_file_name):
+#    inputFile = open(input_file_name, 'r')
+#
+#    for line in inputFile:
+#        line = line.translate(str.maketrans('', '', string.punctuation))
+#
+#
+#
+#   ouputFile = open(output_file_name, 'w')
 
 
 
